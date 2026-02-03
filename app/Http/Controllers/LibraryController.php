1dbd303d6da1f311619a48c3df1ebda7a1eb8798
duncangeddie <duncan@websprout.co.za>
2026-02-03 20:35:58 +0200

    Created library.blade.php topic icons with links

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -10,6 +10,56 @@ class LibraryController extends Controller
     {
         $LibraryPage = [
             'background' => 'library_background.png',
+
+            // Topic 1
+            'topic1_label' => 'Abstract Classes',
+            'topic1_icon' => 'library_images/library_icon_abstract.png',
+            'topic1_link' => '#',
+
+            // Topic 2
+            'topic2_label' => 'Algorithms',
+            'topic2_icon' => 'library_images/library_icon_algorithm.png',
+            'topic2_link' => '#',
+
+            // Topic 3
+            'topic3_label' => 'Asyncio',
+            'topic3_icon' => 'library_images/library_icon_asyncio.png',
+            'topic3_link' => '#',
+
+            // Topic 4
+            'topic4_label' => 'Attributes',
+            'topic4_icon' => 'library_images/library_icon_attributes.png',
+            'topic4_link' => '#',
+
+            // Topic 5
+            'topic5_label' => 'Bitwise Operators',
+            'topic5_icon' => 'library_images/library_icon_bitwise.png',
+            'topic5_link' => '#',
+
+            // Topic 6
+            'topic6_label' => 'Boolean Logic',
+            'topic6_icon' => 'library_images/library_icon_boolean.png',
+            'topic6_link' => '#',
+
+            // Topic 7
+            'topic7_label' => 'Classes',
+            'topic7_icon' => 'library_images/library_icon_classes.png',
+            'topic7_link' => '#',
+
+            // Topic 8
+            'topic8_label' => 'Comprehensions',
+            'topic8_icon' => 'library_images/library_icon_comprehension.png',
+            'topic8_link' => '#',
+
+            // Topic 9
+            'topic9_label' => 'Concurrency',
+            'topic9_icon' => 'library_images/library_icon_concurrency.png',
+            'topic9_link' => '#',
+
+            // Topic 10
+            'topic10_label' => 'Context Managers',
+            'topic10_icon' => 'library_images/library_icon_context.png',
+            'topic10_link' => '#',
         ];
 
         return view('library', compact('LibraryPage'));
--- resources/views/library.blade.php
+++ resources/views/library.blade.php
@@ -6,6 +6,65 @@
         }
     </style>
 
-    <!-- Optional: add content here -->
+    <div class="library-topics">
+        <!-- Topic 1 -->
+        <a href="{{ $LibraryPage['topic1_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic1_icon']) }}" alt="{{ $LibraryPage['topic1_label'] }} Icon">
+            <span>{{ $LibraryPage['topic1_label'] }}</span>
+        </a>
 
+        <!-- Topic 2 -->
+        <a href="{{ $LibraryPage['topic2_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic2_icon']) }}" alt="{{ $LibraryPage['topic2_label'] }} Icon">
+            <span>{{ $LibraryPage['topic2_label'] }}</span>
+        </a>
+
+        <!-- Topic 3 -->
+        <a href="{{ $LibraryPage['topic3_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic3_icon']) }}" alt="{{ $LibraryPage['topic3_label'] }} Icon">
+            <span>{{ $LibraryPage['topic3_label'] }}</span>
+        </a>
+
+        <!-- Topic 4 -->
+        <a href="{{ $LibraryPage['topic4_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic4_icon']) }}" alt="{{ $LibraryPage['topic4_label'] }} Icon">
+            <span>{{ $LibraryPage['topic4_label'] }}</span>
+        </a>
+
+        <!-- Topic 5 -->
+        <a href="{{ $LibraryPage['topic5_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic5_icon']) }}" alt="{{ $LibraryPage['topic5_label'] }} Icon">
+            <span>{{ $LibraryPage['topic5_label'] }}</span>
+        </a>
+
+        <!-- Topic 6 -->
+        <a href="{{ $LibraryPage['topic6_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic6_icon']) }}" alt="{{ $LibraryPage['topic6_label'] }} Icon">
+            <span>{{ $LibraryPage['topic6_label'] }}</span>
+        </a>
+
+        <!-- Topic 7 -->
+        <a href="{{ $LibraryPage['topic7_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic7_icon']) }}" alt="{{ $LibraryPage['topic7_label'] }} Icon">
+            <span>{{ $LibraryPage['topic7_label'] }}</span>
+        </a>
+
+        <!-- Topic 8 -->
+        <a href="{{ $LibraryPage['topic8_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic8_icon']) }}" alt="{{ $LibraryPage['topic8_label'] }} Icon">
+            <span>{{ $LibraryPage['topic8_label'] }}</span>
+        </a>
+
+        <!-- Topic 9 -->
+        <a href="{{ $LibraryPage['topic9_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic9_icon']) }}" alt="{{ $LibraryPage['topic9_label'] }} Icon">
+            <span>{{ $LibraryPage['topic9_label'] }}</span>
+        </a>
+
+        <!-- Topic 10 -->
+        <a href="{{ $LibraryPage['topic10_link'] }}" class="library-topic">
+            <img src="{{ asset($LibraryPage['topic10_icon']) }}" alt="{{ $LibraryPage['topic10_label'] }} Icon">
+            <span>{{ $LibraryPage['topic10_label'] }}</span>
+        </a>
+    </div>
 </x-app-layout>
