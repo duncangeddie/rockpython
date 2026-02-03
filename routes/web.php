98c4fe5103cb0b0fce7ef6b84d19e19ba668e422
duncangeddie <duncan@websprout.co.za>
2026-02-03 14:41:19 +0200

    Updated web.php to set https://rockpython.ddev.site/ route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,20 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', [WelcomeController::class, 'index']);
+// Root route: redirect based on authentication
+Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+    // Call the controller properly (not statically)
+    return (new WelcomeController)->index();
+});
+
+// Auth routes
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 
+// Protected routes (only for signed-in + verified users)
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
