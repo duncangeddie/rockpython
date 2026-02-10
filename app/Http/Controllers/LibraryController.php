979ce48960b9bd14f5f89a7425a1b34a026d1d24
duncangeddie <duncan@websprout.co.za>
2026-02-10 12:18:07 +0200

    routes/web.php - added routes for library topics

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -12,43 +12,43 @@ class LibraryController extends Controller
             'background' => 'library_background.png',
 
             'topic1_label' => 'Abstract Classes',
-            'topic1_icon' => 'library_images/library_icon_abstract.png',
+            'topic1_icon' => 'library_images/library_topic1_icon.png',
             'topic1_link' => '/library/topic1',
 
             'topic2_label' => 'Algorithms',
-            'topic2_icon' => 'library_images/library_icon_algorithm.png',
+            'topic2_icon' => 'library_images/library_topic2_icon.png',
             'topic2_link' => '/library/topic2',
 
             'topic3_label' => 'Asyncio',
-            'topic3_icon' => 'library_images/library_icon_asyncio.png',
+            'topic3_icon' => 'library_images/library_topic3_icon.png',
             'topic3_link' => '/library/topic3',
 
             'topic4_label' => 'Attributes',
-            'topic4_icon' => 'library_images/library_icon_attributes.png',
+            'topic4_icon' => 'library_images/library_topic4_icon.png',
             'topic4_link' => '/library/topic4',
 
             'topic5_label' => 'Bitwise Operators',
-            'topic5_icon' => 'library_images/library_icon_bitwise.png',
+            'topic5_icon' => 'library_images/library_topic5_icon.png',
             'topic5_link' => '/library/topic5',
 
             'topic6_label' => 'Boolean Logic',
-            'topic6_icon' => 'library_images/library_icon_boolean.png',
+            'topic6_icon' => 'library_images/library_topic6_icon.png',
             'topic6_link' => '/library/topic6',
 
             'topic7_label' => 'Classes',
-            'topic7_icon' => 'library_images/library_icon_classes.png',
+            'topic7_icon' => 'library_images/library_topic7_icon.png',
             'topic7_link' => '/library/topic7',
 
             'topic8_label' => 'Comprehensions',
-            'topic8_icon' => 'library_images/library_icon_comprehension.png',
+            'topic8_icon' => 'library_images/library_topic8_icon.png',
             'topic8_link' => '/library/topic8',
 
             'topic9_label' => 'Concurrency',
-            'topic9_icon' => 'library_images/library_icon_concurrency.png',
+            'topic9_icon' => 'library_images/library_topic9_icon.png',
             'topic9_link' => '/library/topic9',
 
             'topic10_label' => 'Context Managers',
-            'topic10_icon' => 'library_images/library_icon_context.png',
+            'topic10_icon' => 'library_images/library_topic10_icon.png',
             'topic10_link' => '/library/topic10',
         ];
 
@@ -60,7 +60,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic1_background.png',
             'label' => 'Abstract Classes',
-            'icon' => 'library_images/library_icon_abstract.png',
+            'icon' => 'library_images/library_topic1_icon.png',
         ];
 
         return view('library-topic1', compact('TopicPage'));
@@ -71,7 +71,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic2_background.png',
             'label' => 'Algorithms',
-            'icon' => 'library_images/library_icon_algorithm.png',
+            'icon' => 'library_images/library_topic2_icon.png',
         ];
 
         return view('library-topic2', compact('TopicPage'));
@@ -82,7 +82,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic3_background.png',
             'label' => 'Asyncio',
-            'icon' => 'library_images/library_icon_asyncio.png',
+            'icon' => 'library_images/library_topic3_icon.png',
         ];
 
         return view('library-topic3', compact('TopicPage'));
@@ -93,7 +93,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic4_background.png',
             'label' => 'Attributes',
-            'icon' => 'library_images/library_icon_attributes.png',
+            'icon' => 'library_images/library_topic4_icon.png',
         ];
 
         return view('library-topic4', compact('TopicPage'));
@@ -104,7 +104,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic5_background.png',
             'label' => 'Bitwise Operators',
-            'icon' => 'library_images/library_icon_bitwise.png',
+            'icon' => 'library_images/library_topic5_icon.png',
         ];
 
         return view('library-topic5', compact('TopicPage'));
@@ -115,7 +115,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic6_background.png',
             'label' => 'Boolean Logic',
-            'icon' => 'library_images/library_icon_boolean.png',
+            'icon' => 'library_images/library_topic6_icon.png',
         ];
 
         return view('library-topic6', compact('TopicPage'));
@@ -126,7 +126,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic7_background.png',
             'label' => 'Classes',
-            'icon' => 'library_images/library_icon_classes.png',
+            'icon' => 'library_images/library_topic7_icon.png',
         ];
 
         return view('library-topic7', compact('TopicPage'));
@@ -137,7 +137,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic8_background.png',
             'label' => 'Comprehensions',
-            'icon' => 'library_images/library_icon_comprehension.png',
+            'icon' => 'library_images/library_topic8_icon.png',
         ];
 
         return view('library-topic8', compact('TopicPage'));
@@ -148,7 +148,7 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic9_background.png',
             'label' => 'Concurrency',
-            'icon' => 'library_images/library_icon_concurrency.png',
+            'icon' => 'library_images/library_topic9_icon.png',
         ];
 
         return view('library-topic9', compact('TopicPage'));
@@ -159,10 +159,9 @@ class LibraryController extends Controller
         $TopicPage = [
             'background' => 'library_topic10_background.png',
             'label' => 'Context Managers',
-            'icon' => 'library_images/library_icon_context.png',
+            'icon' => 'library_images/library_topic10_icon.png',
         ];
 
         return view('library-topic10', compact('TopicPage'));
     }
-
 }
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,74 @@ Route::middleware([
     Route::get('/library/topic8', [LibraryController::class, 'topic8'])->name('library.topic8');
     Route::get('/library/topic9', [LibraryController::class, 'topic9'])->name('library.topic9');
     Route::get('/library/topic10', [LibraryController::class, 'topic10'])->name('library.topic10');
+
+    Route::get('/library/topic11', [LibraryController::class, 'topic11'])->name('library.topic11');
+    Route::get('/library/topic12', [LibraryController::class, 'topic12'])->name('library.topic12');
+    Route::get('/library/topic13', [LibraryController::class, 'topic13'])->name('library.topic13');
+    Route::get('/library/topic14', [LibraryController::class, 'topic14'])->name('library.topic14');
+    Route::get('/library/topic15', [LibraryController::class, 'topic15'])->name('library.topic15');
+    Route::get('/library/topic16', [LibraryController::class, 'topic16'])->name('library.topic16');
+    Route::get('/library/topic17', [LibraryController::class, 'topic17'])->name('library.topic17');
+    Route::get('/library/topic18', [LibraryController::class, 'topic18'])->name('library.topic18');
+    Route::get('/library/topic19', [LibraryController::class, 'topic19'])->name('library.topic19');
+    Route::get('/library/topic20', [LibraryController::class, 'topic20'])->name('library.topic20');
+
+    Route::get('/library/topic21', [LibraryController::class, 'topic21'])->name('library.topic21');
+    Route::get('/library/topic22', [LibraryController::class, 'topic22'])->name('library.topic22');
+    Route::get('/library/topic23', [LibraryController::class, 'topic23'])->name('library.topic23');
+    Route::get('/library/topic24', [LibraryController::class, 'topic24'])->name('library.topic24');
+    Route::get('/library/topic25', [LibraryController::class, 'topic25'])->name('library.topic25');
+    Route::get('/library/topic26', [LibraryController::class, 'topic26'])->name('library.topic26');
+    Route::get('/library/topic27', [LibraryController::class, 'topic27'])->name('library.topic27');
+    Route::get('/library/topic28', [LibraryController::class, 'topic28'])->name('library.topic28');
+    Route::get('/library/topic29', [LibraryController::class, 'topic29'])->name('library.topic29');
+    Route::get('/library/topic30', [LibraryController::class, 'topic30'])->name('library.topic30');
+
+    Route::get('/library/topic31', [LibraryController::class, 'topic31'])->name('library.topic31');
+    Route::get('/library/topic32', [LibraryController::class, 'topic32'])->name('library.topic32');
+    Route::get('/library/topic33', [LibraryController::class, 'topic33'])->name('library.topic33');
+    Route::get('/library/topic34', [LibraryController::class, 'topic34'])->name('library.topic34');
+    Route::get('/library/topic35', [LibraryController::class, 'topic35'])->name('library.topic35');
+    Route::get('/library/topic36', [LibraryController::class, 'topic36'])->name('library.topic36');
+    Route::get('/library/topic37', [LibraryController::class, 'topic37'])->name('library.topic37');
+    Route::get('/library/topic38', [LibraryController::class, 'topic38'])->name('library.topic38');
+    Route::get('/library/topic39', [LibraryController::class, 'topic39'])->name('library.topic39');
+    Route::get('/library/topic40', [LibraryController::class, 'topic40'])->name('library.topic40');
+
+    Route::get('/library/topic41', [LibraryController::class, 'topic41'])->name('library.topic41');
+    Route::get('/library/topic42', [LibraryController::class, 'topic42'])->name('library.topic42');
+    Route::get('/library/topic43', [LibraryController::class, 'topic43'])->name('library.topic43');
+    Route::get('/library/topic44', [LibraryController::class, 'topic44'])->name('library.topic44');
+    Route::get('/library/topic45', [LibraryController::class, 'topic45'])->name('library.topic45');
+    Route::get('/library/topic46', [LibraryController::class, 'topic46'])->name('library.topic46');
+    Route::get('/library/topic47', [LibraryController::class, 'topic47'])->name('library.topic47');
+    Route::get('/library/topic48', [LibraryController::class, 'topic48'])->name('library.topic48');
+    Route::get('/library/topic49', [LibraryController::class, 'topic49'])->name('library.topic49');
+    Route::get('/library/topic50', [LibraryController::class, 'topic50'])->name('library.topic50');
+
+    Route::get('/library/topic51', [LibraryController::class, 'topic51'])->name('library.topic51');
+    Route::get('/library/topic52', [LibraryController::class, 'topic52'])->name('library.topic52');
+    Route::get('/library/topic53', [LibraryController::class, 'topic53'])->name('library.topic53');
+    Route::get('/library/topic54', [LibraryController::class, 'topic54'])->name('library.topic54');
+    Route::get('/library/topic55', [LibraryController::class, 'topic55'])->name('library.topic55');
+    Route::get('/library/topic56', [LibraryController::class, 'topic56'])->name('library.topic56');
+    Route::get('/library/topic57', [LibraryController::class, 'topic57'])->name('library.topic57');
+    Route::get('/library/topic58', [LibraryController::class, 'topic58'])->name('library.topic58');
+    Route::get('/library/topic59', [LibraryController::class, 'topic59'])->name('library.topic59');
+    Route::get('/library/topic60', [LibraryController::class, 'topic60'])->name('library.topic60');
+
+    Route::get('/library/topic61', [LibraryController::class, 'topic61'])->name('library.topic61');
+    Route::get('/library/topic62', [LibraryController::class, 'topic62'])->name('library.topic62');
+    Route::get('/library/topic63', [LibraryController::class, 'topic63'])->name('library.topic63');
+    Route::get('/library/topic64', [LibraryController::class, 'topic64'])->name('library.topic64');
+    Route::get('/library/topic65', [LibraryController::class, 'topic65'])->name('library.topic65');
+    Route::get('/library/topic66', [LibraryController::class, 'topic66'])->name('library.topic66');
+    Route::get('/library/topic67', [LibraryController::class, 'topic67'])->name('library.topic67');
+    Route::get('/library/topic68', [LibraryController::class, 'topic68'])->name('library.topic68');
+    Route::get('/library/topic69', [LibraryController::class, 'topic69'])->name('library.topic69');
+    Route::get('/library/topic70', [LibraryController::class, 'topic70'])->name('library.topic70');
+    Route::get('/library/topic71', [LibraryController::class, 'topic71'])->name('library.topic71');
+    Route::get('/library/topic72', [LibraryController::class, 'topic72'])->name('library.topic72');
+    Route::get('/library/topic73', [LibraryController::class, 'topic73'])->name('library.topic73');
+    Route::get('/library/topic74', [LibraryController::class, 'topic74'])->name('library.topic74');
 });
